da4ec705721397a03c474f3a7e72017a02f5f5cd
MasaG6014 <g2002masa@gmail.com>
2025-01-23 17:09:51 +0900

    Modify BACKENDURL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,2 +1,2 @@
 // export const BACKEND_URL = "http://localhost:8000";
-export const BACKEND_URL = "https://a32c-133-9-96-38.ngrok-free.app";
+export const BACKEND_URL = "https://0bfb-133-9-96-38.ngrok-free.app";
